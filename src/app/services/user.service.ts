c8d6322f9e8e75eff9905af2fbfc8a82ddb9bafe
Jiten Pandey <devendrap@flock.com>
2021-06-17 23:14:59 +0530

    fixed registration page

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,6 +14,6 @@ export class UserService {
   //add user
   public addUser(user: any)
   {
-    return this.http.post(`${baseUrl}/user/`,user);
+    return this.http.post(`${baseUrl}/user/add/`,user);
   }
 }
